217c9750fe5b7e1dfe4d1130d1d7a605e357b324
Steve Lavoie <steven.lavoiejr@gmail.com>
2015-07-19 13:49:19 -1000

    updated solution/project names and directories to be consistent

---
+++ SkaaEditorUI/SkaaMainForm.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SkaaEditor
+{
+    public partial class SkaaMainForm : Form
+    {
+        public SkaaMainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ResourceFile resfile = new ResourceFile("i_menu2.res");
+            //pictureBox1.Image = resfile.Resources[0].initIMGStream();
+
+            FileStream spritestream = File.OpenRead("../../data/sprite/ballista.spr");
+            //ballista.spr length = 82578 (data_buf_size in init_import()
+
+            /************************
+             * Get sprite header data 
+             ************************/
+            //Byte[] sprite_data_size_bytes = new Byte[4];
+            //spritestream.Read(sprite_data_size_bytes, 0, 4);
+            //int sprite_data_size = BitConverter.ToInt32(sprite_data_size_bytes,0);
+
+            /************************
+             * Get all of the frames
+             ************************/
+            //Byte[] sprite_data = new Byte[spritestream.Length];
+            //spritestream.Read(sprite_data, 0, (int) spritestream.Length);
+
+
+            List<SpriteFrame> sprFrames = new List<SpriteFrame>();
+
+            while (spritestream.Position < spritestream.Length)
+            {
+                ////jump to offset 17537, w=62, h=65 (cur_dir=1) + 4 byte (junk?)
+                //spritestream.Seek(17537+4, SeekOrigin.Begin);
+                Byte[] frame_size_bytes = new Byte[4];
+                spritestream.Seek(4, SeekOrigin.Current); //skip the 32-bit size value
+                spritestream.Read(frame_size_bytes, 0, 4);
+
+                short width = BitConverter.ToInt16(frame_size_bytes, 0);// >> 16;
+                short height = BitConverter.ToInt16(frame_size_bytes, 2);// >> 16;
+
+                SpriteFrame frame = new SpriteFrame(width, height);
+
+                frame.GetPixels(spritestream);
+
+                //debugging: gives an ASCII representation of image 
+                //(add \n after every 62d character). Verified alignment of pixels as read.
+                //var hex = BitConverter.ToString(frame.FrameData);
+
+                frame.BuildBitmap();
+                sprFrames.Add(frame);
+                pictureBox1.Image = sprFrames[0].Images[0];
+                
+                //end early, just get one to test
+                //spritestream.Position = spritestream.Length;
+            }
+
+
+
+            int zoomWidth = sprFrames[0].Images[0].Width * 1;
+            int zoomHeight = sprFrames[0].Images[0].Height * 1;
+            System.Drawing.Bitmap bmp = 
+                new System.Drawing.Bitmap(sprFrames[0].Images[0], new System.Drawing.Size(zoomWidth, zoomHeight));
+            
+            // TODO: Just a hack since we skip pixels that are preset to 0x00.
+            // Will need to write those pixels as the actual Color.Transparent
+            // so we can have black in our images.
+            bmp.MakeTransparent(System.Drawing.Color.Black);
+            
+            pictureBox1.Image = bmp; // sprFrames[0].Images[0];
+            spritestream.Close();
+        }
+    }
+}
---
+++ SkaaEditorUI/SpriteFrame.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace SkaaEditor
+{
+    public static class Helper
+    {
+        public static ColorPalette LoadPalette()
+        {
+            ColorPalette pal_std = new Bitmap(50, 50, PixelFormat.Format8bppIndexed).Palette;// = new ColorPalette();
+
+            FileStream fs = File.OpenRead("../../data/resource/pal_std.res");
+            fs.Seek(8, SeekOrigin.Begin);
+
+            for (int i = 0; i < 256; i++)
+            {
+                int r = fs.ReadByte();
+                int g = fs.ReadByte();
+                int b = fs.ReadByte();
+
+                pal_std.Entries[i] = Color.FromArgb(255, r, g, b);
+            }
+
+            return pal_std;
+        }
+    }
+
+    public class Sprite
+    {
+
+    }
+
+    public class SpriteFrame
+    {
+        public int Height
+        {
+            get;
+            set;
+        }
+        public int Width
+        {
+            get;
+            set;
+        }
+        public Byte[] FrameData
+        {
+            get;
+            set;
+        }
+        public List<Bitmap> Images
+        {
+            get;
+            set;
+        }
+
+        public SpriteFrame(int width, int height)
+        {
+            this.Height = height;
+            this.Width = width;
+
+            Images = new List<Bitmap>();
+            FrameData = new Byte[height * width];
+        }
+
+        public void GetPixels(FileStream stream)
+        {
+            int pixelsToSkip = 0;
+            Byte pixel;
+
+            for (int y = 0; y < this.Height; ++y)
+            {
+                for (int x = 0; x < this.Width; ++x)
+                {
+                    if (pixelsToSkip != 0)
+                    {
+                        if (pixelsToSkip >= this.Width - x)
+                        {
+                            pixelsToSkip -= (this.Width - x); // skip to next line
+                            break;
+                        }
+
+                        x += pixelsToSkip;
+                        pixelsToSkip = 0;
+                    }
+
+                    try { pixel = Convert.ToByte(stream.ReadByte()); }
+                    catch { return; /*got -1 for EOS*/ }
+
+                    if (pixel < 0xf8)//MIN_TRANSPARENT_CODE) //normal pixel
+                    {
+                        this.FrameData[this.Width * y + x] = pixel;
+                    }
+                    else if (pixel == 0xf8)//MANY_TRANSPARENT_CODE)
+                    {
+                        pixelsToSkip = stream.ReadByte() - 1;
+                    }
+                    else //f9,fa,fb,fc,fd,fe,ff
+                    {
+                        pixelsToSkip = 256 - pixel - 1;	// skip (neg al) pixels
+                    }
+                }//end inner for
+            }//end outer for
+        }//end GetPixels()
+        public void BuildBitmap()
+        {
+            Bitmap bmp = new Bitmap(this.Width, this.Height);//, PixelFormat.Format8bppIndexed);
+            Color[] pal = Helper.LoadPalette().Entries;
+
+            for (int y = 0; y < this.Height; y++)
+            {
+                for (int x = 0; x < this.Width; x++)
+                {                    
+                    Color pixel = pal[FrameData[y * this.Width + x]];
+                    bmp.SetPixel(x, y, pixel);
+                    bmp.SetPixel(x, y, Color.FromArgb(255, pixel));
+                    
+                }
+            }
+            Images.Add(bmp);
+        }//end BuildBitmap()
+    }
+}
