558c8d118e4c584f6ebd369fcc2530499d5e5276
Steve Lavoie <steven.lavoiejr@gmail.com>
2015-11-13 22:09:30 -1000

    removed old junk code

--- SkaaGameDataLib/GameSetFile.cs
+++ SkaaGameDataLib/GameSetFile.cs
@@ -18,7 +18,7 @@ namespace SkaaGameDataLib
          * ResourceIndex[recordCount]
          * Database[recordCount]
          */
-        private string /*_setFileName, _setFileDirectory,*/ _tempDirectory, _dbfDirectory, _setFilePath;
+        private string _tempDirectory, _dbfDirectory, _setFilePath;
         private short _recordCount;
         private const int _rowNameSize = 9;
         private const int _resIndexSize = 13;
@@ -35,8 +35,6 @@ namespace SkaaGameDataLib
         /// <param name="tempDirectoryPath">The complete path to a temporary directory the class can use freely.</param>
         public GameSetFile(string setFilePath, string tempDirectoryPath)
         {
-            //this._setFileName = Path.GetFileName(setFilePath);
-            //this._setFileDirectory = Path.GetDirectoryName(setFilePath);
             this._setFilePath = setFilePath;
             this._tempDirectory = tempDirectoryPath;
             this._dbfDirectory = this._tempDirectory + "dbf\\";
@@ -113,18 +111,7 @@ namespace SkaaGameDataLib
 
                 DataTable dt;
 
-                //string connex = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" +
-                //                this._dbfDirectory + 
-                //                ";Extended Properties=dBase III";
-
-                //using (OleDbConnection dbConnection = new OleDbConnection(connex))
-                //{
-                //    OleDbCommand cmd = new OleDbCommand("SELECT * FROM [" + dbfFileName + ']', dbConnection);
-
-                //    using (OleDbDataAdapter adapter = new OleDbDataAdapter(cmd))
-                //    {
-
-                        dt = new DataTable(Path.GetFileNameWithoutExtension(dbfFileName));
+                dt = new DataTable(Path.GetFileNameWithoutExtension(dbfFileName));
 
                 using (MemoryStream ms = new MemoryStream())
                 {
@@ -134,8 +121,6 @@ namespace SkaaGameDataLib
                     db.DbfFileObject = new DbfFile(ms, dt.TableName, (int) ms.Length);
                     dt = db.DbfFileObject.FillAndGetTable();
                 }
-                //    }
-                //}
 
                 db.Table = dt;
                 ds.Tables.Add(db.Table);
@@ -169,7 +154,7 @@ namespace SkaaGameDataLib
                     //writes out individual DBFs... easier to inspect SFRAME, etc
                     string path = this._dbfDirectory + "test";
                     Directory.CreateDirectory(path);
-                    db.DbfFileObject.WriteAndClose(path);                   
+                    db.DbfFileObject.WriteAndClose(path);
 #endif
                     db.DbfFileObject.WriteToStream(fs);
                 }
@@ -178,84 +163,5 @@ namespace SkaaGameDataLib
                 fs.Write(BitConverter.GetBytes(fileSize), 0, 4);
             }
         }
-
-        #region deprecated
-        //private void WriteSframe(FileStream fs, GameSet set)
-        //{
-        //    //write individual databases
-        //    DataTable dt = set.GameDataSet.Tables["SFRAME"];
-        //    foreach (DataRow dr in dt.Rows)
-        //    {
-        //        fs.WriteByte(0x20); //row divider
-        //        for (int i = 0; i < dr.ItemArray.Count(); i++)
-        //        {
-        //            byte[] bytes;
-        //            switch (i)
-        //            {
-        //                case 0: //char[8] SPRITE
-        //                    string sprName = (dr[i] == DBNull.Value) ? string.Empty : (string) dr[i];
-        //                    sprName = sprName.Length < 8 ? sprName.PadRight(8, (char) 0x20) : sprName;
-        //                    bytes = Encoding.GetEncoding(1252).GetBytes(sprName);
-        //                    fs.Write(bytes, 0, bytes.Length);
-        //                    break;
-        //                case 1: //char[2] ACTION
-        //                    string action = (dr[i] == DBNull.Value) ? "0" : (string) dr[i];
-        //                    action = action.Length < 2 ? action.PadRight(2, (char) 0x20) : action;
-        //                    bytes = Encoding.GetEncoding(1252).GetBytes(action);
-        //                    fs.Write(bytes, 0, bytes.Length);
-        //                    break;
-        //                case 2: //char[2] DIR
-        //                    string dir = (dr[i] == DBNull.Value) ? "0" : (string) dr[i];
-        //                    dir = dir.Length < 2 ? dir.PadRight(2, (char) 0x20) : dir;
-        //                    bytes = Encoding.GetEncoding(1252).GetBytes(dir);
-        //                    fs.Write(bytes, 0, bytes.Length);
-        //                    break;
-        //                case 3: //char[2] FRAME
-        //                    string frame = (dr[i] == DBNull.Value) ? string.Empty : (string) dr[i];
-        //                    frame = frame.Length < 2 ? frame.PadRight(2, (char) 0x20) : frame;
-        //                    bytes = Encoding.GetEncoding(1252).GetBytes(frame);
-        //                    fs.Write(bytes, 0, bytes.Length);
-        //                    break;
-        //                case 4: //char[4] OFFSET_X
-        //                    string offset_x = (dr[i] == DBNull.Value) ? string.Empty : ((int) ((long) dr[i])).ToString();
-        //                    offset_x = offset_x.Length < 4 ? offset_x.PadLeft(4, (char) 0x20) : offset_x;
-        //                    bytes = Encoding.GetEncoding(1252).GetBytes(offset_x);
-        //                    fs.Write(bytes, 0, bytes.Length);
-        //                    break;
-        //                case 5: //char[4] OFFSET_Y
-        //                    string offset_y = (dr[i] == DBNull.Value) ? string.Empty : ((int) ((long) dr[i])).ToString();
-        //                    offset_y = offset_y.Length < 4 ? offset_y.PadLeft(4, (char) 0x20) : offset_y;
-        //                    bytes = Encoding.GetEncoding(1252).GetBytes(offset_y);
-        //                    fs.Write(bytes, 0, bytes.Length);
-        //                    break;
-        //                case 6: //char[3] WIDTH
-        //                    string width = (dr[i] == DBNull.Value) ? string.Empty : ((int) ((long) dr[i])).ToString();
-        //                    width = width.Length < 3 ? width.PadLeft(3, (char) 0x20) : width;
-        //                    bytes = Encoding.GetEncoding(1252).GetBytes(width);
-        //                    fs.Write(bytes, 0, bytes.Length);
-        //                    break;
-        //                case 7: //char[3] HEIGHT
-        //                    string height = (dr[i] == DBNull.Value) ? string.Empty : ((int) ((long) dr[i])).ToString();
-        //                    height = height.Length < 3 ? height.PadLeft(3, (char) 0x20) : height;
-        //                    bytes = Encoding.GetEncoding(1252).GetBytes(height);
-        //                    fs.Write(bytes, 0, bytes.Length);
-        //                    break;
-        //                case 8: //char[8] FILENAME
-        //                    string filename = (dr[i] == DBNull.Value) ? string.Empty : (string) dr[i];
-        //                    filename = filename.Length < 8 ? filename.PadRight(8, (char) 0x20) : filename;
-        //                    bytes = Encoding.GetEncoding(1252).GetBytes(filename);
-        //                    fs.Write(bytes, 0, bytes.Length);
-        //                    break;
-        //                case 9: //char[4] BITMAPPTR
-        //                    int ptr = (dr[i] == DBNull.Value) ? 0 : Convert.ToInt32((string) dr[i]);
-        //                    bytes = BitConverter.GetBytes(ptr);
-        //                    fs.Write(bytes, 0, bytes.Length);
-        //                    break;
-        //            }
-        //        }
-        //    }
-        //    fs.WriteByte(0x1a); //EOF
-        //}
-        #endregion
     }
 }
