0816724e094c0513c50d30ae8bedba28961b5ee7
Steve Lavoie <steven.lavoiejr@gmail.com>
2015-11-14 16:22:52 -1000

    removed old HandToolIcon that wasn't used

--- Capslock.WinForms.ImageEditor/Properties/Resources.Designer.cs
+++ Capslock.WinForms.ImageEditor/Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace Capslock.WinForms.ImageEditor.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap HandToolIcon {
-            get {
-                object obj = ResourceManager.GetObject("HandToolIcon", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap PaintBucketIcon {
             get {
                 object obj = ResourceManager.GetObject("PaintBucketIcon", resourceCulture);
