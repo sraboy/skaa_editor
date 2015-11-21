90934c919f37cd9d3dbfe97126330dacc73641cd
Steve Lavoie <steven.lavoiejr@gmail.com>
2015-11-21 13:23:22 -1000

    updated icon, fixed AboutForm bug by changing Show() to ShowDialog() and updated its image

--- SkaaEditorUI/AboutForm.Designer.cs
+++ SkaaEditorUI/AboutForm.Designer.cs
@@ -53,12 +53,13 @@ namespace SkaaEditorUI
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.lklblAuthor = new System.Windows.Forms.LinkLabel();
             this.lnklblSKFans = new System.Windows.Forms.LinkLabel();
             this.pbLogo = new System.Windows.Forms.PictureBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
             this.SuspendLayout();
             // 
@@ -66,47 +67,36 @@ namespace SkaaEditorUI
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(89, 14);
+            this.label1.Location = new System.Drawing.Point(88, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(148, 18);
+            this.label1.Size = new System.Drawing.Size(111, 18);
             this.label1.TabIndex = 0;
-            this.label1.Text = "SkaaEditor  v0.1 ALPHA";
+            this.label1.Text = "SkaaEditor  v0.4a";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(88, 30);
+            this.label2.Location = new System.Drawing.Point(88, 28);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(150, 18);
+            this.label2.Size = new System.Drawing.Size(141, 18);
             this.label2.TabIndex = 1;
-            this.label2.Text = "(C) 2015, Steven Lavoie";
+            this.label2.Text = "© 2015 Steven Lavoie";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(88, 46);
+            this.label3.Location = new System.Drawing.Point(88, 43);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(145, 18);
             this.label3.TabIndex = 2;
             this.label3.Text = "Released under GLPv3";
             // 
-            // lklblAuthor
-            // 
-            this.lklblAuthor.AutoSize = true;
-            this.lklblAuthor.Location = new System.Drawing.Point(145, 97);
-            this.lklblAuthor.Name = "lklblAuthor";
-            this.lklblAuthor.Size = new System.Drawing.Size(131, 13);
-            this.lklblAuthor.TabIndex = 3;
-            this.lklblAuthor.TabStop = true;
-            this.lklblAuthor.Text = "steven.lavoejr@gmail.com";
-            this.lklblAuthor.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lklblAuthor_LinkClicked);
-            // 
             // lnklblSKFans
             // 
             this.lnklblSKFans.AutoSize = true;
-            this.lnklblSKFans.Location = new System.Drawing.Point(156, 111);
+            this.lnklblSKFans.Location = new System.Drawing.Point(88, 75);
             this.lnklblSKFans.Name = "lnklblSKFans";
             this.lnklblSKFans.Size = new System.Drawing.Size(120, 13);
             this.lnklblSKFans.TabIndex = 4;
@@ -115,6 +105,7 @@ namespace SkaaEditorUI
             // 
             // pbLogo
             // 
+            this.pbLogo.Image = global::SkaaEditorUI.Properties.Resources.Kazuya_Hartless;
             this.pbLogo.Location = new System.Drawing.Point(12, 12);
             this.pbLogo.Name = "pbLogo";
             this.pbLogo.Size = new System.Drawing.Size(73, 83);
@@ -122,17 +113,28 @@ namespace SkaaEditorUI
             this.pbLogo.TabIndex = 5;
             this.pbLogo.TabStop = false;
             // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(88, 60);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(131, 13);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://github.com/sraboy";
+            // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(280, 127);
+            this.ClientSize = new System.Drawing.Size(276, 127);
+            this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.pbLogo);
             this.Controls.Add(this.lnklblSKFans);
-            this.Controls.Add(this.lklblAuthor);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AboutForm";
@@ -148,8 +150,8 @@ namespace SkaaEditorUI
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.LinkLabel lklblAuthor;
         private System.Windows.Forms.LinkLabel lnklblSKFans;
         private System.Windows.Forms.PictureBox pbLogo;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
--- SkaaEditorUI/Properties/Resources.Designer.cs
+++ SkaaEditorUI/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace SkaaEditorUI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Kazuya_Hartless {
+            get {
+                object obj = ResourceManager.GetObject("Kazuya-Hartless", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap MenuFileCloseIcon {
             get {
                 object obj = ResourceManager.GetObject("MenuFileCloseIcon", resourceCulture);
