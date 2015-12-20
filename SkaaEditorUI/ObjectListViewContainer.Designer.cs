aa1cb0bcd71f8ab0a4b23e25a85938307597fc9e
Steve Lavoie <steven.lavoiejr@gmail.com>
2015-12-19 15:11:00 -1000

    Add ObjectListViewContainer test form
    Simply shows an ObjectListView, anchored to the form. Call SetDataSource()
    to set its DataSource to an IListSource (like DataTable). It currently
    works for the SFRAME table in std.set. It needs to be flexible and will
    be used to allow the user to select which files they're going to save and
    how, when more than one data source is used for ActiveGameSet.

---
+++ SkaaEditorUI/ObjectListViewContainer.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace SkaaEditorUI
+{
+    partial class ObjectListViewContainer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataListView1 = new BrightIdeasSoftware.DataListView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataListView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataListView1
+            // 
+            this.dataListView1.CellEditUseWholeCell = false;
+            this.dataListView1.DataSource = null;
+            this.dataListView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataListView1.HighlightBackgroundColor = System.Drawing.Color.Empty;
+            this.dataListView1.HighlightForegroundColor = System.Drawing.Color.Empty;
+            this.dataListView1.Location = new System.Drawing.Point(0, 0);
+            this.dataListView1.Name = "dataListView1";
+            this.dataListView1.Size = new System.Drawing.Size(847, 485);
+            this.dataListView1.TabIndex = 0;
+            this.dataListView1.UseCompatibleStateImageBehavior = false;
+            this.dataListView1.View = System.Windows.Forms.View.Details;
+            // 
+            // ObjectListViewContainer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 485);
+            this.Controls.Add(this.dataListView1);
+            this.Name = "ObjectListViewContainer";
+            this.Text = "ObjectListViewContainer";
+            ((System.ComponentModel.ISupportInitialize)(this.dataListView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private BrightIdeasSoftware.DataListView dataListView1;
+    }
+}
---
+++ SkaaEditorUI/ObjectListViewContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SkaaEditorUI
+{
+    public partial class ObjectListViewContainer : Form
+    {
+        public ObjectListViewContainer()
+        {
+            InitializeComponent();
+        }
+
+        public void SetDataSource<T>(T dataSource) where T : IListSource
+        {
+            this.dataListView1.DataSource = dataSource;
+        }
+    }
+}
