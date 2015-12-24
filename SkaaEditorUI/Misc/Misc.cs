61ef0f810c7ead5d33e2b3c7b37251c1b3d5013e
Steve Lavoie <steven.lavoiejr@gmail.com>
2015-12-23 16:17:04 -1000

    Move Utility Classes to Misc namespace

---
+++ SkaaEditorUI/Misc/Misc.cs
@@ -0,0 +1,52 @@
+﻿#region Copyright Notice
+/***************************************************************************
+* The MIT License (MIT)
+*
+* Copyright © 2015-2016 Steven Lavoie
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of
+* this software and associated documentation files (the "Software"), to deal in
+* the Software without restriction, including without limitation the rights to
+* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+* the Software, and to permit persons to whom the Software is furnished to do so,
+* subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+* IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+***************************************************************************/
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkaaEditorUI
+{
+    public static class Misc
+    {
+        public static string GetDesignModeValue<T>(Expression<Func<T>> propertyExpression)
+        {
+            //adapted from: http://stackoverflow.com/questions/4364888/how-to-get-property-name-from-within-getter-setter-of-that-property
+            return (propertyExpression.Body as MemberExpression).Member.Name;
+        }
+
+        public static bool SetField<T>(ref T field, T value, Action onPropertyChanged)
+        {
+            //adapted from: http://stackoverflow.com/questions/4364888/how-to-get-property-name-from-within-getter-setter-of-that-property
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+            field = value;
+            onPropertyChanged();
+            return true;
+        }
+    }
+}
--- SkaaEditorUI/Misc/TrulyObservableCollection.cs
+++ SkaaEditorUI/Misc/TrulyObservableCollection.cs
@@ -1,9 +1,33 @@
-﻿using System;
+﻿#region Copyright Notice
+/***************************************************************************
+* The MIT License (MIT)
+*
+* Copyright © 2015-2016 Steven Lavoie
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of
+* this software and associated documentation files (the "Software"), to deal in
+* the Software without restriction, including without limitation the rights to
+* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+* the Software, and to permit persons to whom the Software is furnished to do so,
+* subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+* FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+* COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+* IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+***************************************************************************/
+#endregion
+using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Collections.Specialized;
 
-namespace SkaaEditorUI
+namespace SkaaEditorUI.Misc
 {
     /// <summary>
     /// A custom implementation of <see cref="ObservableCollection{T}"/> that raises events for collection member changes by enforcing
@@ -11,9 +35,9 @@ namespace SkaaEditorUI
     /// </summary>
     /// <typeparam name="T">Any type that implements <see cref="INotifyPropertyChanged"/></typeparam>
     /// <remarks>
-    /// This class is licensed under CC-BY-SA as it was adapted from a StackOverflow user post. See:
-    /// http://stackoverflow.com/questions/1427471/observablecollection-not-noticing-when-item-in-it-changes-even-with-inotifyprop/5256827#5256827
-    /// License: CC-BY-SA (http://creativecommons.org/licenses/by-sa/2.5/)
+    /// This specific class is licensed under CC-BY-SA v2.5 as it was adapted from a StackOverflow user post. 
+    /// Original: http://stackoverflow.com/questions/1427471/observablecollection-not-noticing-when-item-in-it-changes-even-with-inotifyprop/5256827#5256827
+    /// License: CC-BY-SA v2.5 (http://creativecommons.org/licenses/by-sa/2.5/)
     /// </remarks>
     [Serializable]
     public class TrulyObservableCollection<T> : ObservableCollection<T>, INotifyPropertyChanged where T : INotifyPropertyChanged
